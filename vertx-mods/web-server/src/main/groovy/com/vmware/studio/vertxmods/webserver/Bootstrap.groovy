e00021403519c39dacf3211edbc713e16e442568
samueldoyle <samueldoyle@vmware.com>
2014-02-10 23:59:25 -0800

    Getting rid of vertx gradle template, better without for multiproject Testing Done: Issue Number: Approved by: Reviewed by:

--- vertx-mods/web-server/src/main/groovy/com/vmware/studio/vertxmods/webserver/Bootstrap.groovy
+++ vertx-mods/web-server/src/main/groovy/com/vmware/studio/vertxmods/webserver/Bootstrap.groovy
@@ -4,19 +4,19 @@ import org.vertx.groovy.platform.Verticle
 class Bootstrap extends Verticle {
 
     def eb = vertx.eventBus
-    def pa = vertx.mongopersistor
+    def pa = "vertx.mongopersistor"
 
     def start() {
-        eb.send(pa, [action: 'delete', collection: 'users', matcher: [:]]) {
+        eb.send(pa, [action: "delete", collection: "users", matcher: [:]]) {
             eb.send(pa, [
-                    action    : 'save',
-                    collection: 'users',
+                    action    : "save",
+                    collection: "users",
                     document  : [
-                            firstname: 'Sam',
-                            lastname : 'Doyle',
-                            email    : 'samuledoyle@lvmware.com',
-                            username : 'sam',
-                            password : 'ca$hc0w'
+                            firstname: "Sam",
+                            lastname : "Doyle",
+                            email    : "samuledoyle@lvmware.com",
+                            username : "sam",
+                            password : "password"
                     ]
             ])
         }
