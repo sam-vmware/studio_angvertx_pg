71337152d5abe37aa41c996d643ea92e8d27bfc3
samueldoyle <samueldoyle@vmware.com>
2014-02-11 14:58:20 -0800

    Initial commit for systemservice Testing Done: Issue Number: Approved by: Reviewed by:

---
+++ vami-mods/system-service/src/main/groovy/com/vmware/studio/vamimods/system/SystemService.groovy
@@ -0,0 +1,36 @@
+package com.vmware.studio.vamimods.system
+
+import org.vertx.groovy.platform.Verticle
+
+class SystemService extends Verticle {
+
+    def authManagerConf = [
+            // addres: 'vertx.basicauthmanager',
+            // persistor_address: 'vertx.mongopersistor',
+            user_collection: 'users',
+            session_timeout: 1800000 // 30mins
+    ]
+
+    def mongoPersistorConf = [
+            host   : 'localhost',
+            port   : 27017,
+            db_name: 'studio_db'
+    ]
+
+    def webServerConf = [
+            //host              : 'localhost',
+            host              : '0.0.0.0',
+            port              : 8080,
+            web_root: '/home/samueldoyle/Projects/VMware/StudioRepo/git/NewStack/studio_angular_vertx/web-ui/app',
+            index_page: 'index.html',
+            static_files      : true,
+            ssl               : false,
+            bridge            : true,
+            inbound_permitted : [[:]],
+            outbound_permitted: [[:]]
+    ]
+
+    def start() {
+        container.log.info "!!!! Hello From SystemService !!!!"
+    }
+}
